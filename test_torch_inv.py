b8557aa1f54e426275d2697da9e188392e384047
Aimin <huangepn@gmail.com>
2023-06-25 20:24:12 +0800

    Update test_torch_inv.py

--- test_torch_inv.py
+++ test_torch_inv.py
@@ -2,8 +2,7 @@ import torch
 import numpy as np
 
 
-
-def test_inv(dtype):
+def test_inv(dtype, dtype_str):
     x = np.load('test_x.npy')
     x = x.astype(dtype)
     x = torch.from_numpy(x)
@@ -12,8 +11,9 @@ def test_inv(dtype):
     x2 = np.linalg.inv(x.numpy())
 
     err = np.max(np.abs(x2 - x2t.numpy()))
+    np.save('test_np_%s_%s.npy' % (np.__version__, dtype_str), x2)
 
     print('err:', dtype, err)
 
-test_inv(np.float32)
-test_inv(np.float64)
+test_inv(np.float32, 'float32')
+test_inv(np.float64, 'float64')
